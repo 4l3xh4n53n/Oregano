f252c98a749f795694240221b3388eb72f18bdf0
4l3xh4n53n <omgalex2006@gmail.com>
2024-08-13 22:19:46 +0000

    Removed debug code and changed invalid command type formatting in DisplaySettings.java

--- src/main/com/4legs/java/Commands/Settings/DisplaySettings.java
+++ src/main/com/4legs/java/Commands/Settings/DisplaySettings.java
@@ -37,7 +37,6 @@ public class DisplaySettings extends AdministrativeCommand {
     }
 
     private static String showRoles(String[] features, String guildID, Guild guild, boolean filtered, String[] filter) {
-
         StringBuilder output = new StringBuilder();
 
         for (String featureName : features) {
@@ -50,17 +49,14 @@ public class DisplaySettings extends AdministrativeCommand {
                 }
             }
 
-            if (SettingsManager.commandUsesPermissions(guildID, featureName)) {
-
-                // Displays Discord's built-in permission
+            if (SettingsManager.commandUsesPermissions(guildID, featureName)) { // Displays Discord's built-in permission
 
                 output.append(featureName)
                         .append(" : ")
                         .append(CommandHandler.getCommandByName(featureName).getBuiltInPermission())
                         .append("\n");
-            } else {
 
-                // Displays all role names
+            } else { // Displays all role names
 
                 String[] roleIDs = SettingsManager.getRequiredRoles(guildID, featureName);
                 StringBuilder roleNames = new StringBuilder();
@@ -85,17 +81,13 @@ public class DisplaySettings extends AdministrativeCommand {
         }
 
         return output.toString();
-
     }
 
     private static String showFeatures(String[] features, String guildID, boolean filtered, String[] filter) {
         StringBuilder output = new StringBuilder();
-
         boolean featureEnabled;
 
         for (String featureName : features) {
-            System.out.println("Feature name: " + featureName);
-
             if (filtered) {
                 CommandType commandType = CommandHandler.getCommandByName(featureName).getType();
 
@@ -111,7 +103,6 @@ public class DisplaySettings extends AdministrativeCommand {
         }
 
         return output.toString();
-
     }
 
     private static String[][] createFilter(String[] args, int argStart) {
@@ -128,7 +119,6 @@ public class DisplaySettings extends AdministrativeCommand {
             } else {
                 unknownFilterTypes.add(filterItem);
             }
-
         }
 
         String[] arrFilter = filter.toArray(String[]::new);
@@ -143,37 +133,28 @@ public class DisplaySettings extends AdministrativeCommand {
         StringBuilder output = new StringBuilder();
         String result;
 
-        // TEST VALUES
-        SettingsManager.setRequiredRoles(guildID, "ban", new String[]{"850115918486044753", "849020974493728848"});
-        SettingsManager.setRequiredRoles(guildID, "kick", new String[]{"850115918486044753"});
-        SettingsManager.setCommandUsesPermission(guildID, "purge", true);
-        SettingsManager.setGuildsFeature(guildID, "purge", true);
-
         if (args.length >= 1){
 
             // Shows permissions/roles required to use a feature with/without filter
             if (args[0].toLowerCase(Locale.ROOT).equals("roles")) {
 
                 if (args.length < 3) { // No filter
-                    System.out.println("ROLES NO FILTER");
-
                     result = showRoles(features, guildID, guild, false, new String[]{});
                 } else { // Filter
-                    System.out.println("ROLES WITH FILTER");
 
                     String[][] createFilterResult = createFilter(args, 1);
                     String[] filter = createFilterResult[0];
                     String[] unknownFeatureTypes = createFilterResult[1];
                     if (unknownFeatureTypes.length > 0) {
                         output.append("Unknown feature types: ")
-                                .append(Arrays.toString(unknownFeatureTypes))
+                                .append(Arrays.toString(unknownFeatureTypes).replace("[", "").replace("]", ""))
                                 .append("\n");
                     }
 
                     result = showRoles(features, guildID, guild, true, filter);
+
                 }
             } else { // Shows whether a feature is on or off with filter
-                System.out.println("TOGGLE WITH FILTER");
 
                 String[][] createFilterResult = createFilter(args, 0);
                 String[] filter = createFilterResult[0];
@@ -188,12 +169,10 @@ public class DisplaySettings extends AdministrativeCommand {
 
             }
         } else { // Displays whether a feature is on or off without filter
-            System.out.println("TOGGLE NO FILTER");
             result = showFeatures(features, guildID, false, new String[]{});
         }
 
         output.append(result);
-
         return output.toString();
     }
 
