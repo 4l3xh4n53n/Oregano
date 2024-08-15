6e2c8238489bb9f4cc50883604e2dea4278966a5
4l3xh4n53n <omgalex2006@gmail.com>
2024-08-15 16:52:50 +0000

    Changed toggle feature to set feature

--- src/main/com/4legs/java/Commands/Settings/SetFeature.java
+++ src/main/com/4legs/java/Commands/Settings/SetFeature.java
@@ -7,11 +7,11 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class ToggleFeature extends SettingsCommand{
+public class SetFeature extends SettingsCommand{
 
     @Override
     public String getExample() {
-        return "togglefeature {FEATURE NAME}";
+        return "setfeature {FEATURE NAME} {on/off or 1/0}";
     }
 
     @Override
@@ -20,17 +20,24 @@ public class ToggleFeature extends SettingsCommand{
     }
 
     @Override
-    public String onCommand(MessageReceivedEvent e, Message message, Guild guild, String guildID, String[] args) { // todo test
+    public String onCommand(MessageReceivedEvent e, Message message, Guild guild, String guildID, String[] args) {
 
-        if (args.length < 1) return "You have not included enough args for this command";
+        if (args.length < 2) return "You have not included enough args for this command";
         Member author = e.getMember();
         if (!author.hasPermission(Permission.ADMINISTRATOR)) return "You do not have permission to run this command.";
 
         Boolean featureStatus = SettingsManager.featureIsEnabled(guildID, args[0]);
         if (featureStatus == null) return "Feature does not exist";
-        SettingsManager.setGuildsFeature(guildID, args[0], !featureStatus);
 
-        return args[0] + " set to: " + !featureStatus;
+        String option = args[1];
+
+        if (option.equals("on") || option.equals("1")) featureStatus = true;
+        else if (option.equals("off") || option.equals("0")) featureStatus = false;
+        else return "Unknown option: " + option;
+
+        SettingsManager.setGuildsFeature(guildID, args[0], featureStatus);
+
+        return args[0] + " set to: " + (featureStatus ? "on" : "off");
 
     }
 
