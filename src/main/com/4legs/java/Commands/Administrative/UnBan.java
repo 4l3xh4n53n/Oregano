64118003dc13d3306ff03f21e57480b0d25aa835
4l3xh4n53n <omgalex2006@gmail.com>
2024-08-21 22:08:59 +0000

    Added AdministrativeChecksAndDate and make checks function for all administrative commands. Added getReason function.

--- src/main/com/4legs/java/Commands/Administrative/UnBan.java
+++ src/main/com/4legs/java/Commands/Administrative/UnBan.java
@@ -1,13 +1,11 @@
 package Commands.Administrative;
 
-import Configurations.SettingsManager;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class UnBan extends AdministrativeCommand{
+public class UnBan extends Ban {
 
     @Override
     public String getExample() {
@@ -26,16 +24,11 @@ public class UnBan extends AdministrativeCommand{
 
     @Override
     public String onCommand(MessageReceivedEvent e, Message message, Guild guild, String guildID, String[] args){
+        AdministrativeChecksAndData c = checks(e, message, guild, guildID, "ban", args, Permission.BAN_MEMBERS, 1, false);
+        if (!c.checksSuccessful()) return c.checksMessage();
 
-        if (!SettingsManager.featureIsEnabled(guildID, "ban")) return null;
-        if (args.length < 1) return "You have not included enough arguments to use this command.";
-        Member mentioned = getMentioned(guild, message, args[0]);
-        if (mentioned == null) return "Member cannot be found.";
-        Member author = e.getMember();
-        boolean hasPermission = checkPermissions(guildID, "ban", author, mentioned, Permission.BAN_MEMBERS);
-        if (!hasPermission) return "You do not have permission to use this command.";
-        guild.unban(mentioned).queue();
-        return "Member has been un-banned";
+        guild.unban(c.mentioned()).queue();
+        return "Member has been un-banned.";
 
     }
 
